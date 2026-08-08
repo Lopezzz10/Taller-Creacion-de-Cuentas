e1a76cfced10cf9c9240cffe804e2d0fa3aee633
Lopezzz10 <josemanuellopezolivez@gmail.com>
2026-08-07 22:50:19 -0500

    Configuración de la Estructura y Implementación de la Clase Cuenta

---
+++ src/clearminds/cuentas/Cuenta.java
@@ -0,0 +1,28 @@
+package clearminds.cuentas;
+
+public class Cuenta {
+
+    private String id;
+    private String tipo = "A";
+    private double saldo;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
